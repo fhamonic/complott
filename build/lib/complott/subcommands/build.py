63e681eef06b2eb2593783761fd52ab8dd6d0b7c
fhamonic <francois.hamonic@gmail.com>
2025-01-23 01:17:26 +0100

    gitignore

--- build/lib/complott/subcommands/build.py
+++
@@ -1,7 +0,0 @@
-import click
-
-@click.command()
-@click.option('--name', '-n', default='World', help='Name to greet')
-def command(name):
-    """A sample subcommand"""
-    click.echo(f"Hello, {name}!")
